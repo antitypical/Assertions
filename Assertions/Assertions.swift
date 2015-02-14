7b71f5b43b8679689b392537be113fa614b2b7c3
Rob Rix <rob.rix@github.com>
2015-02-14 15:40:50 -0500

    All the assertions are public.

--- Assertions/Assertions.swift
+++ Assertions/Assertions.swift
@@ -5,7 +5,7 @@
 /// Asserts the equality of two Equatable values.
 ///
 /// Returns the value, if equal and non-nil.
-func assertEqual<T: Equatable>(@autoclosure expression1: () -> T?, @autoclosure expression2: () -> T?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> T? {
+public func assertEqual<T: Equatable>(@autoclosure expression1: () -> T?, @autoclosure expression2: () -> T?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> T? {
 	let (actual, expected) = (expression1(), expression2())
 	return actual == expected ? actual : failure("\(actual) is not equal to \(expected). " + message, file: file, line: line)
 }
@@ -14,7 +14,7 @@ func assertEqual<T: Equatable>(@autoclosure expression1: () -> T?, @autoclosure 
 /// Asserts the equality of two arrays of Equatable values.
 ///
 /// Returns the array, if equal and non-nil.
-func assertEqual<T: Equatable>(@autoclosure expression1: () -> [T]?, @autoclosure expression2: () -> [T]?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> [T]? {
+public func assertEqual<T: Equatable>(@autoclosure expression1: () -> [T]?, @autoclosure expression2: () -> [T]?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> [T]? {
 	let (actual, expected) = (expression1(), expression2())
 	switch (actual, expected) {
 	case (.None, .None):
@@ -30,7 +30,7 @@ func assertEqual<T: Equatable>(@autoclosure expression1: () -> [T]?, @autoclosur
 /// Asserts the equality of two dictionaries of Equatable values.
 ///
 /// Returns the dictionary, if equal and non-nil.
-func assertEqual<T: Hashable, U: Equatable>(@autoclosure expression1: () -> [T: U]?, @autoclosure expression2: () -> [T: U]?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> [T: U]? {
+public func assertEqual<T: Hashable, U: Equatable>(@autoclosure expression1: () -> [T: U]?, @autoclosure expression2: () -> [T: U]?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> [T: U]? {
 	let (actual, expected) = (expression1(), expression2())
 	switch (actual, expected) {
 	case (.None, .None):
@@ -46,12 +46,12 @@ func assertEqual<T: Hashable, U: Equatable>(@autoclosure expression1: () -> [T: 
 // MARK: - Nil/non-nil
 
 /// Asserts that a value is nil.
-func assertNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> Bool {
+public func assertNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> Bool {
 	return expression().map { failure("\($0) is not nil. " + message, file: file, line: line) } ?? true
 }
 
 /// Asserts that a value is not nil.
-func assertNotNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> T? {
+public func assertNotNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> T? {
 	return expression() ?? failure("is nil. " + message, file: file, line: line)
 }
 
@@ -61,7 +61,7 @@ func assertNotNil<T>(@autoclosure expression: () -> T?, _ message: String = "", 
 /// Logs a failed assertion.
 ///
 /// Returns nil, for use in `x ?? failure(…)` expressions.
-func failure<T>(message: String, file: String = __FILE__, line: UInt = __LINE__) -> T? {
+public func failure<T>(message: String, file: String = __FILE__, line: UInt = __LINE__) -> T? {
 	XCTFail(message, file: file, line: line)
 	return nil
 }
@@ -69,7 +69,7 @@ func failure<T>(message: String, file: String = __FILE__, line: UInt = __LINE__)
 /// Logs a failed assertion.
 ///
 /// Returns nil, for use in `x ?? failure(…)` expressions.
-func failure(message: String, file: String = __FILE__, line: UInt = __LINE__) -> Bool {
+public func failure(message: String, file: String = __FILE__, line: UInt = __LINE__) -> Bool {
 	XCTFail(message, file: file, line: line)
 	return false
 }
