5f5dfbf1ac442fd1a0eafe6af1516bd3c4cd723b
Rob Rix <rob.rix@github.com>
2015-02-14 15:40:12 -0500

    Docs.

--- Assertions/Assertions.swift
+++ Assertions/Assertions.swift
@@ -2,12 +2,18 @@
 
 // MARK: - Equality
 
+/// Asserts the equality of two Equatable values.
+///
+/// Returns the value, if equal and non-nil.
 func assertEqual<T: Equatable>(@autoclosure expression1: () -> T?, @autoclosure expression2: () -> T?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> T? {
 	let (actual, expected) = (expression1(), expression2())
 	return actual == expected ? actual : failure("\(actual) is not equal to \(expected). " + message, file: file, line: line)
 }
 
 
+/// Asserts the equality of two arrays of Equatable values.
+///
+/// Returns the array, if equal and non-nil.
 func assertEqual<T: Equatable>(@autoclosure expression1: () -> [T]?, @autoclosure expression2: () -> [T]?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> [T]? {
 	let (actual, expected) = (expression1(), expression2())
 	switch (actual, expected) {
@@ -20,6 +26,10 @@ func assertEqual<T: Equatable>(@autoclosure expression1: () -> [T]?, @autoclosur
 	}
 }
 
+
+/// Asserts the equality of two dictionaries of Equatable values.
+///
+/// Returns the dictionary, if equal and non-nil.
 func assertEqual<T: Hashable, U: Equatable>(@autoclosure expression1: () -> [T: U]?, @autoclosure expression2: () -> [T: U]?, _ message: String = "", _ file: String = __FILE__, _ line: UInt = __LINE__) -> [T: U]? {
 	let (actual, expected) = (expression1(), expression2())
 	switch (actual, expected) {
@@ -35,11 +45,12 @@ func assertEqual<T: Hashable, U: Equatable>(@autoclosure expression1: () -> [T: 
 
 // MARK: - Nil/non-nil
 
+/// Asserts that a value is nil.
 func assertNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> Bool {
 	return expression().map { failure("\($0) is not nil. " + message, file: file, line: line) } ?? true
-
 }
 
+/// Asserts that a value is not nil.
 func assertNotNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> T? {
 	return expression() ?? failure("is nil. " + message, file: file, line: line)
 }
@@ -47,11 +58,17 @@ func assertNotNil<T>(@autoclosure expression: () -> T?, _ message: String = "", 
 
 // MARK: - Failure
 
+/// Logs a failed assertion.
+///
+/// Returns nil, for use in `x ?? failure(…)` expressions.
 func failure<T>(message: String, file: String = __FILE__, line: UInt = __LINE__) -> T? {
 	XCTFail(message, file: file, line: line)
 	return nil
 }
 
+/// Logs a failed assertion.
+///
+/// Returns nil, for use in `x ?? failure(…)` expressions.
 func failure(message: String, file: String = __FILE__, line: UInt = __LINE__) -> Bool {
 	XCTFail(message, file: file, line: line)
 	return false
