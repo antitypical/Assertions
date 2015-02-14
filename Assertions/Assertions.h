f19222c85b74b45ab8535912e484f1aa30980813
Rob Rix <rob.rix@github.com>
2015-02-14 15:12:16 -0500

    Initial Commit

---
+++ Assertions/Assertions.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 Rob Rix. All rights reserved.
+
+/// Project version number for Assertions.
+extern double AssertionsVersionNumber;
+
+/// Project version string for Assertions.
+extern const unsigned char AssertionsVersionString[];
+
