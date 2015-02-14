1e406ab3b22e88a9e5bb825402fc888bdc16e3de
Rob Rix <rob.rix@github.com>
2015-02-14 15:35:06 -0500

    Nil/non-nil assertions.

--- Assertions/Assertions.swift
+++ Assertions/Assertions.swift
@@ -1,5 +1,17 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+// MARK: - Nil/non-nil
+
+func assertNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> Bool {
+	return expression().map { failure("\($0) is not nil. " + message, file: file, line: line) } ?? true
+
+}
+
+func assertNotNil<T>(@autoclosure expression: () -> T?, _ message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> T? {
+	return expression() ?? failure("is nil. " + message, file: file, line: line)
+}
+
+
 // MARK: - Failure
 
 func failure<T>(message: String, file: String = __FILE__, line: UInt = __LINE__) -> T? {
