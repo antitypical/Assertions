7d3839d56f5d65e3da15c23326d52888f448f2d8
Rob Rix <rob.rix@github.com>
2015-02-14 15:34:42 -0500

    Failure assertions.

--- Assertions/Assertions.swift
+++ Assertions/Assertions.swift
@@ -1,5 +1,18 @@
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
 
+// MARK: - Failure
+
+func failure<T>(message: String, file: String = __FILE__, line: UInt = __LINE__) -> T? {
+	XCTFail(message, file: file, line: line)
+	return nil
+}
+
+func failure(message: String, file: String = __FILE__, line: UInt = __LINE__) -> Bool {
+	XCTFail(message, file: file, line: line)
+	return false
+}
+
+
 // MARK: - Imports
 
 import XCTest
