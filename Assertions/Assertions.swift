a451583e07e3a81449d565a60a154700abd70aa6
Rob Rix <rob.rix@github.com>
2015-02-23 19:50:59 -0500

    Non-optional assertions.

--- Assertions/Assertions.swift
+++ Assertions/Assertions.swift
@@ -7,7 +7,7 @@
 /// This is useful for asserting the equality of collections which only define equality for Equatable element types.
 ///
 ///		assert(x, ==, y)
-public func assert<T, U>(@autoclosure expression1: () -> T?, test: (T, U) -> Bool, @autoclosure expression2: () -> U?, message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> T? {
+public func assert<T, U>(@autoclosure expression1: () -> T, test: (T, U) -> Bool, @autoclosure expression2: () -> U, message: String = "", file: String = __FILE__, line: UInt = __LINE__) -> T? {
 	return assertExpected(expression1(), test, expression2(), message, file, line)
 }
 
